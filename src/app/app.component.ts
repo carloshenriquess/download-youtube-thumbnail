89bd7a39e1d9f6f4224a6e122bef59eb2c935dca
Carlos Henrique <carlos.henrique0307200@gmail.com>
2020-05-20 01:06:03 -0300

    estrutura inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { fromEvent } from 'rxjs';
+import { filter, map, distinctUntilChanged } from 'rxjs/operators';
+
+const getMaxresThumbnailUrl = (videoUrl: string) => {
+  return videoUrl
+    .replace('www.youtube', 'i.ytimg')
+    .replace('watch?v=', 'vi/')
+    .concat('/maxresdefault.jpg');
+};
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'download-youtube-thumbnail';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('form') form: ElementRef<HTMLFormElement>;
+  formGroup: FormGroup;
+  loading = false;
+  image$: Promise<string>;
+  constructor(
+    private formBuilder: FormBuilder,
+  ){}
+
+  ngOnInit() {
+    this.buildForm();
+  }
+  ngAfterViewInit() {
+    this.setupSubmitListener();
+  }
+
+  private setupSubmitListener() {
+    const url$ = fromEvent(this.form.nativeElement, 'submit').pipe(
+      filter(() => this.formGroup.valid),
+      map(() => this.formGroup.value.url),
+      map(getMaxresThumbnailUrl),
+      distinctUntilChanged(),
+    );
+
+    url$.subscribe(url => {
+      this.loading = true;
+      const img = new Image();
+      img.src = url;
+      img.onload = () => {
+        this.image$ = Promise.resolve(url);
+        this.loading = false;
+      }
+    })
+  }
+
+  private buildForm() {
+    this.formGroup = this.formBuilder.group({
+      url: [null, Validators.required]
+    });
+  }
+
 }
